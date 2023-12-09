acbee40ebcaaa6d9e6cd06dd8cae053258fe8d4a
Hilliam Tung <hilliamtung@gmail.com>
2023-12-09 11:50:09 +0000

    implement deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,13 +3,24 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "../src/AoriVault.sol";
+import {ICREATE3Factory} from "create3-factory/src/ICREATE3Factory.sol";
 
 contract DeployScript is Script {
     function run() external {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+        address deployerAddress = vm.addr(deployerPrivateKey);
+        address create3FactoryAddress = vm.envAddress("CREATE3FACTORY_ADDRESS");
+        address aoriProtocolAddress = vm.envAddress("AORIPROTOCOL_ADDRESS");
 
         vm.startBroadcast(deployerPrivateKey);
-        // TODO: 
+
+        ICREATE3Factory(create3FactoryAddress).deploy(
+            keccak256(bytes("if youre reading this, trade on aori thx")),
+            abi.encodePacked(
+                type(AoriVault).creationCode,
+                abi.encode(deployerAddress, aoriProtocolAddress)
+            )
+        );
 
         vm.stopBroadcast();
     }
